447a4e32e454e9b4104d72530ea1463aac61a477
Allaz Adam <aallaz.pro@gmail.com>
2024-05-28 01:45:46 +0200

    Continuing page.js

--- page.js
+++ page.js
@@ -10,5 +10,38 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("mouseout", function() {
             button.style.backgroundColor = "rgb(84, 136, 136)";
         });
+
+        $('#lutherie').click(function () {
+            var content = $('#content');
+            content.empty();
+
+            var html = `
+            <h1 class="title">Lutherie</h1>`
+            ;
+
+            content.append(html);
+        });
+
+        $('#musique').click(function () {
+            var content = $('#content');
+            content.empty();
+
+            var html = `
+            <h1 class="title">Musique</h1>`
+            ;
+
+            content.append(html);
+        });
+
+        $('#code').click(function () {
+            var content = $('#content');
+            content.empty();
+
+            var html = `
+            <h1 class="title">Code / Informatique</h1>`
+            ;
+
+            content.append(html);
+        });
     });
 });
