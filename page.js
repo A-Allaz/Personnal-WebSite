02b037487eedb5c09631e0e3f13acbb3ef1edf4e
Allaz Adam <aallaz.pro@gmail.com>
2024-05-28 02:18:13 +0200

    Added 'contact'.

--- page.js
+++ page.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    //Menu
     var buttons = document.querySelectorAll(".menuButton");
 
     buttons.forEach(function(button) {
@@ -10,38 +11,72 @@ document.addEventListener("DOMContentLoaded", function() {
         button.addEventListener("mouseout", function() {
             button.style.backgroundColor = "rgb(84, 136, 136)";
         });
-
-        $('#lutherie').click(function () {
-            var content = $('#content');
-            content.empty();
-
-            var html = `
-            <h1 class="title">Lutherie</h1>`
-            ;
-
-            content.append(html);
-        });
-
-        $('#musique').click(function () {
-            var content = $('#content');
-            content.empty();
-
-            var html = `
-            <h1 class="title">Musique</h1>`
-            ;
-
-            content.append(html);
-        });
-
-        $('#code').click(function () {
-            var content = $('#content');
-            content.empty();
-
-            var html = `
-            <h1 class="title">Code / Informatique</h1>`
-            ;
-
-            content.append(html);
-        });
     });
+
+    //Contact
+    var contact = document.querySelector('.contactButton');
+
+    if(contact){
+        contact.addEventListener("mouseover", function() {
+            contact.style.color = "white";
+        });
+        contact.addEventListener("mouseout", function() {
+            contact.style.color = "black";
+        });
+    }
+
+    //Lutherie
+    $('#lutherie').click(function () {
+        var content = $('#content');
+        content.empty();
+
+        var html = `
+        <h1 class="title">Lutherie</h1>`
+        ;
+
+        content.append(html);
+    });
+
+    //Musique
+    $('#musique').click(function () {
+        var content = $('#content');
+        content.empty();
+
+        var html = `
+        <h1 class="title">Musique</h1>`
+        ;
+
+        content.append(html);
+    });
+
+    //Code / Informatique
+    $('#code').click(function () {
+        var content = $('#content');
+        content.empty();
+
+        var html = `
+        <h1 class="title">Code / Informatique</h1>`
+        ;
+
+        content.append(html);
+    });
+
+    //Contact
+    $('#contact').click(function () {
+        var content = $('#content');
+        content.empty();
+
+        var html = `
+        <h1 class="title">Contact</h1>
+        <div class="contactDiv">
+            <h3><u>Mail :</u></h3>
+            <p>exemple@mail.com</p>
+            <h3><u>Téléphone :</u></h3>
+            <p>01 02 03 04 05</p>
+        </div>
+        `;
+
+        content.append(html);
+    });
+    
 });
